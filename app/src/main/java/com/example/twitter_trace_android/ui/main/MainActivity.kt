061db53b76e99a0bf988b8b80dc1449406650742
Ren <rentoytoy0828@gmai.com>
2022-05-10 11:58:51 +0900

    背景色が全体に反映されるように修正

--- app/src/main/java/com/example/twitter_trace_android/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/twitter_trace_android/ui/main/MainActivity.kt
@@ -3,7 +3,13 @@ package com.example.twitter_trace_android.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
 import com.example.twitter_trace_android.ui.TwitterTraceApp
+import com.example.twitter_trace_android.ui.theme.TwitterTraceAndroidTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -14,7 +20,16 @@ class MainActivity : ComponentActivity() {
 //        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            TwitterTraceApp()
+            TwitterTraceAndroidTheme(darkTheme = true) {
+                Box(
+                    modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.background)
+                ){
+                    TwitterTraceApp()
+                }
+
+            }
         }
     }
 }
